d9cf92f2cfa311810e4c908ba8f36baa9fceff4e
Bugen Zhao <i@bugenzhao.com>
2022-09-30 20:02:17 +0800

    unify rustflags
    Signed-off-by: Bugen Zhao <i@bugenzhao.com>

--- ci/scripts/build-simulation.sh
+++ ci/scripts/build-simulation.sh
@@ -9,7 +9,7 @@ echo "--- Generate RiseDev CI config"
 cp ci/risedev-components.ci.env risedev-components.user.env
 
 echo "--- Build deterministic simulation e2e test runner"
-cargo make sslt --profile ci-release -- --help
+ENABLE_RELEASE_PROFILE=true cargo make sslt -- --help
 
 echo "--- Upload artifacts"
 cp target/sim/ci-release/risingwave_simulation ./risingwave_simulation
