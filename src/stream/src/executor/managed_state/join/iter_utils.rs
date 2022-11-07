7cd79b0415b3f89939f740fdd04d25d458bae25e
Alex Chi <iskyzh@gmail.com>
2022-11-06 20:48:50 -0500

    fix(build): revert toolchain workarounds in risingwave_stream
    Signed-off-by: Alex Chi <iskyzh@gmail.com>

--- src/stream/src/executor/managed_state/join/iter_utils.rs
+++ src/stream/src/executor/managed_state/join/iter_utils.rs
@@ -19,14 +19,14 @@ use futures::future::join;
 use futures::{pin_mut, Stream, StreamExt};
 use futures_async_stream::try_stream;
 use risingwave_common::array::Row;
-use risingwave_storage::error::StorageError;
+use risingwave_storage::error::{StorageError, StorageResult};
 
 /// Zip two streams of primary key and rows into a single stream, sorted by order key.
 /// We should ensure that the order key from different streams are unique.
 #[try_stream(ok = (Cow<'a, Row>, Cow<'a, Row>), error = StorageError)]
 pub async fn zip_by_order_key<'a, S>(stream1: S, stream2: S)
 where
-    S: Stream<Item = (Cow<'a, Vec<u8>>, Cow<'a, Row>)> + 'a,
+    S: Stream<Item = StorageResult<(Cow<'a, Vec<u8>>, Cow<'a, Row>)>> + 'a,
 {
     let (stream1, stream2) = (stream1.peekable(), stream2.peekable());
     pin_mut!(stream1);
@@ -36,7 +36,7 @@ where
         match join(stream1.as_mut().peek(), stream2.as_mut().peek()).await {
             (None, _) | (_, None) => break,
 
-            (Some((left_key, _)), Some((right_key, _))) => match left_key.cmp(right_key) {
+            (Some(Ok((left_key, _))), Some(Ok((right_key, _)))) => match left_key.cmp(right_key) {
                 Ordering::Greater => {
                     stream2.next().await;
                 }
@@ -44,11 +44,20 @@ where
                     stream1.next().await;
                 }
                 Ordering::Equal => {
-                    let row_l = stream1.next().await.unwrap().1;
-                    let row_r = stream2.next().await.unwrap().1;
+                    let row_l = stream1.next().await.unwrap()?.1;
+                    let row_r = stream2.next().await.unwrap()?.1;
                     yield (row_l, row_r);
                 }
             },
+
+            (Some(Err(_)), Some(_)) => {
+                // Throw the left error.
+                return Err(stream1.next().await.unwrap().unwrap_err());
+            }
+            (Some(_), Some(Err(_))) => {
+                // Throw the right error.
+                return Err(stream2.next().await.unwrap().unwrap_err());
+            }
         }
     }
 }
@@ -60,11 +69,11 @@ mod tests {
 
     use super::*;
 
-    fn gen_row_with_pk(i: i64) -> (Cow<'static, Vec<u8>>, Cow<'static, Row>) {
-        (
+    fn gen_row_with_pk(i: i64) -> StorageResult<(Cow<'static, Vec<u8>>, Cow<'static, Row>)> {
+        Ok((
             Cow::Owned(i.to_be_bytes().to_vec()),
             Cow::Owned(Row(vec![Some(ScalarImpl::Int64(i))])),
-        )
+        ))
     }
 
     #[tokio::test]
@@ -90,7 +99,7 @@ mod tests {
         #[for_await]
         for (i, result) in zipped.enumerate() {
             let (res0, res1) = result.unwrap();
-            let expected_res = gen_row_with_pk(expected_results[i]);
+            let expected_res = gen_row_with_pk(expected_results[i]).unwrap();
             assert_eq!(res0, expected_res.1);
             assert_eq!(res1, expected_res.1);
         }
