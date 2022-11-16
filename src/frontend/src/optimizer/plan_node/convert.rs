e997b80530790c861d05739d1495e9c34e282ff6
ice1000 <ice1000kotlin@foxmail.com>
2022-11-16 02:10:06 +0000

    Squashed commit of the following:
    commit 7c35f73f02a59031f179e974ab70883fa67d179f
    Author: st1page <1245835950@qq.com>
    Date:   Fri Nov 4 14:49:53 2022 +0800
    
        conflict
    
    commit c33ea35f51b3fccfd5dd1a1bea9087406822368c
    Merge: 859650a18 227e9e5ac
    Author: st1page <1245835950@qq.com>
    Date:   Fri Nov 4 14:43:07 2022 +0800
    
        Merge remote-tracking branch 'origin/main' into sts/introduce_new_stream_node
    
    commit 859650a184bf5da773db4af7959c63b196225088
    Author: st1page <1245835950@qq.com>
    Date:   Tue Nov 1 15:18:32 2022 +0800
    
        add dist enforce for the new stream node
    
    commit b0a5c9342eec4b0c1f56459bea6f2d6ffce38b91
    Author: st1page <1245835950@qq.com>
    Date:   Tue Nov 1 13:43:21 2022 +0800
    
        introduce new stream node

--- src/frontend/src/optimizer/plan_node/convert.rs
+++ src/frontend/src/optimizer/plan_node/convert.rs
@@ -47,6 +47,22 @@ pub trait ToStream {
         let ret = self.to_stream()?;
         required_dist.enforce_if_not_satisfies(ret, &Order::any())
     }
+
+    // **Remove Before Merge**
+    // a temporary function for refactor
+    fn to_stream_v2(&self) -> Result<stream::PlanRef> {
+        unimplemented!();
+    }
+
+    // **Remove Before Merge**
+    // a temporary function for refactor
+    fn to_stream_with_dist_required_v2(
+        &self,
+        required_dist: &RequiredDist,
+    ) -> Result<stream::PlanRef> {
+        let ret = self.to_stream_v2()?;
+        Ok(required_dist.enforce_stream_if_not_satisfies(ret))
+    }
 }
 
 /// `ToBatch` allows to convert a logical plan node to batch physical node
