dd7d12f0284d8826c9fb0fde93c12ae81d9d53f9
weili <a320_271N@outlook.com>
2023-04-25 17:04:14 +0800

    fix

--- java/connector-node/risingwave-connector-service/src/main/java/com/risingwave/sourcenode/types/CdcChunk.java
+++ java/connector-node/risingwave-connector-service/src/main/java/com/risingwave/sourcenode/types/CdcChunk.java
@@ -1,10 +1,16 @@
 package com.risingwave.sourcenode.types;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class CdcChunk {
     long sourceId;
     List<CdcMessage> messages;
+
+    public CdcChunk() {
+        this.sourceId = 0;
+        this.messages = new ArrayList<CdcMessage>();
+    }
 
     public long getSourceId() {
         return sourceId;
