beb6e964e1c424eb0a589fbd144695d4fd7dfef4
Renjie Liu <liurenjie2008@gmail.com>
2023-08-11 10:34:38 +0800

    Check sql

--- integration_tests/iceberg-sink2/python/check.py
+++ integration_tests/iceberg-sink2/python/check.py
@@ -1,31 +1,22 @@
 from pyspark.sql import SparkSession
-import csv
-import argparse
-import unittest
+import configparser
 
 
-def check(args):
-    tc = unittest.TestCase()
-    spark = SparkSession.builder.remote(args.sparkurl).getOrCreate()
+def init_spark_table(args):
+    spark_config = args['spark']
+    spark = SparkSession.builder.remote(spark_config['url']).getOrCreate()
 
-    sql = "SELECT * FROM s1.t1 ORDER BY id ASC"
-    print(f"Executing sql: {sql}")
-    df = spark.sql(sql).collect()
-    for row in df:
-        print(row)
+    sqls = [
+        "SELECT COUNT(*) FROM s1.t1"
+    ]
 
-    with open(args.file, newline='') as insert_csv_file:
-        csv_result = csv.reader(insert_csv_file)
-        for (row1, row2) in zip(df, csv_result):
-            print(f"Row1: {row1}, row 2: {row2}")
-            tc.assertEqual(row1[0], int(row2[0]))
-            tc.assertEqual(row1[1], row2[1])
-            tc.assertEqual(row1[2], int(row2[2]))
+    for sql in sqls:
+        print(f"Executing sql: {sql}")
+        result = spark.sql(sql).collect()
+        print(f"Result is {result}")
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description='Test icelake with spark')
-    parser.add_argument('-s', dest='sparkurl', type=str, help='Spark remote url')
-    parser.add_argument("-f", dest='file', type=str, help='Path to query csv file')
-
-    check(parser.parse_args())
+    config = configparser.ConfigParser()
+    config.read("config.ini")
+    print({section: dict(config[section]) for section in config.sections()})
