2c775b6eeca75e7e297b120a9f48a70add01d694
Little-Wallace <bupt2013211450@gmail.com>
2024-03-07 18:44:22 +0800

    limit trivial task count

--- src/meta/src/hummock/manager/mod.rs
+++ src/meta/src/hummock/manager/mod.rs
@@ -1125,6 +1125,7 @@ impl HummockManager {
             .with_label_values(&["get_compact_task"])
             .start_timer();
 
+        let mut remain_count = 60;
         while let Some(task) = self
             .get_compact_task_impl(compaction_group_id, selector)
             .await?
@@ -1136,6 +1137,10 @@ impl HummockManager {
                 CompactStatus::is_trivial_move_task(&task)
                     || CompactStatus::is_trivial_reclaim(&task)
             );
+            remain_count -= 1;
+            if remain_count == 0 {
+                break;
+            }
         }
 
         Ok(None)
